24e023f77c6df652335ed830af1a14c7e936ab57
EC2 Default User <ec2-user@ip-172-31-38-76.ap-northeast-1.compute.internal>
2023-03-18 03:43:50 +0000

    2023/03/18

--- script/mypage.js
+++ script/mypage.js
@@ -14,3 +14,17 @@ $(function(){
     $event.preventDefault;
   });
 });
+
+const targets = document.getElementsByClassName('post');
+for(let i = targets.length; i--;){
+ let observer = new IntersectionObserver((entries, observer) => {
+  for(let j = entries.length; j--;){
+   if (entries[j].isIntersecting) {
+    entries[j].target.classList.add('active');
+   } else{
+    entries[j].target.classList.remove('active');
+   }
+  }
+ });
+ observer.observe(targets[i]);
+}
